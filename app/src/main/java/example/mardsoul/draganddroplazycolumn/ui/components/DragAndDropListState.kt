d5b5a6d2681f669410dd6815f401c1d194e21be4
Aleksei Laptev <mar.d.soul.app@gmail.com>
2025-02-02 15:06:26 +0400

    Step 3 (Part 2)

--- app/src/main/java/example/mardsoul/draganddroplazycolumn/ui/components/DragAndDropListState.kt
+++ app/src/main/java/example/mardsoul/draganddroplazycolumn/ui/components/DragAndDropListState.kt
@@ -15,115 +15,136 @@ import kotlinx.coroutines.channels.Channel
 
 @Composable
 fun rememberDragAndDropListState(
-	lazyListState: LazyListState,
-	onMove: (Int, Int) -> Unit
+    lazyListState: LazyListState,
+    onMove: (Int, Int) -> Unit
 ): DragAndDropListState {
-	val state = remember { DragAndDropListState(lazyListState, onMove) }
-	LaunchedEffect(state) {
-		while (true) {
-			val diff = state.scrollChannel.receive()
-			state.lazyListState.scrollBy(diff)
-		}
-	}
-	return state
+    val state = remember { DragAndDropListState(lazyListState, onMove) }
+    LaunchedEffect(state) {
+        while (true) {
+            val diff = state.scrollChannel.receive()
+            state.lazyListState.scrollBy(diff)
+        }
+    }
+    return state
 }
 
 class DragAndDropListState(
-	val lazyListState: LazyListState,
-	private val onMove: (Int, Int) -> Unit
+    val lazyListState: LazyListState,
+    private val onMove: (Int, Int) -> Unit
 ) {
-	val scrollChannel = Channel<Float>()
+    // channel for emitting scroll events
+    val scrollChannel = Channel<Float>()
 
-	private var draggingDistance by mutableFloatStateOf(0f)
-	private var initialDraggingElement by mutableStateOf<LazyListItemInfo?>(null)
-	var currentIndexOfDraggedItem by mutableStateOf<Int?>(null)
-	private val initialOffsets: Pair<Int, Int>?
-		get() = initialDraggingElement?.let { Pair(it.offset, it.offsetEnd) }
-	val elementDisplacement: Float?
-		get() = currentIndexOfDraggedItem?.let {
-			lazyListState.getVisibleItemInfo(it)
-		}?.let { itemInfo ->
-			(initialDraggingElement?.offset ?: 0f).toFloat() + draggingDistance - itemInfo.offset
-		}
-	private val currentElement: LazyListItemInfo?
-		get() = currentIndexOfDraggedItem?.let {
-			lazyListState.getVisibleItemInfo(it)
-		}
+    // the index of item that is being dragged
+    var currentIndexOfDraggedItem by mutableStateOf<Int?>(null)
+        private set
 
-	fun onDragStart(offset: Offset) {
-		lazyListState.layoutInfo.visibleItemsInfo
-			.firstOrNull { item -> offset.y.toInt() in item.offset..item.offsetEnd }
-			?.also {
-				initialDraggingElement = it
-				currentIndexOfDraggedItem = it.index
-			}
-	}
+    // the item that is being dragged
+    private val currentElement: LazyListItemInfo?
+        get() = currentIndexOfDraggedItem?.let { lazyListState.getVisibleItemInfo(it) }
 
-	fun onDragInterrupted() {
-		initialDraggingElement = null
-		currentIndexOfDraggedItem = null
-		draggingDistance = 0f
-	}
+    // the initial index of item that is being dragged
+    private var initialDraggingElement by mutableStateOf<LazyListItemInfo?>(null)
 
-	fun onDrag(offset: Offset) {
-		draggingDistance += offset.y
+    // the initial offset of the item when it is dragged
+    private val initialOffsets: Pair<Int, Int>?
+        get() = initialDraggingElement?.let { Pair(it.offset, it.offsetEnd) }
 
-		initialOffsets?.let { (top, bottom) ->
-			val startOffset = top.toFloat() + draggingDistance
-			val endOffset = bottom.toFloat() + draggingDistance
+    // distance that has been dragged
+    private var draggingDistance by mutableFloatStateOf(0f)
 
-			currentElement?.let { current ->
-				val targetElement = lazyListState.layoutInfo.visibleItemsInfo
-					.filterNot { item ->
-						item.offsetEnd < startOffset || item.offset > endOffset || current.index == item.index
-					}
-					.firstOrNull { item ->
-						val delta = startOffset - current.offset
-						when {
-							delta < 0 -> item.offset > startOffset
-							else -> item.offsetEnd < endOffset
-						}
-					}
-				if (targetElement == null) {
-					checkOverscroll()
-				}
-				targetElement
-			}?.also { item ->
-				currentIndexOfDraggedItem?.let { current ->
-					onMove.invoke(current, item.index)
-				}
-				currentIndexOfDraggedItem = item.index
-			}
-		}
-	}
+    // calculates the vertical displacement of the dragged element
+    // relative to its original position in the LazyList.
+    val elementDisplacement: Float?
+        get() = currentIndexOfDraggedItem
+            ?.let { lazyListState.getVisibleItemInfo(it) }
+            ?.let { itemInfo ->
+                (initialDraggingElement?.offset
+                    ?: 0f).toFloat() + draggingDistance - itemInfo.offset
+            }
 
-	private fun checkOverscroll() {
-		val overscroll = initialDraggingElement?.let {
-			val startOffset = it.offset + draggingDistance
-			val endOffset = it.offsetEnd + draggingDistance
 
-			return@let when {
-				draggingDistance > 0 -> {
-					(endOffset - lazyListState.layoutInfo.viewportEndOffset).takeIf { diff -> diff > 0 }
-				}
+    /**
+     * Starts the dragging operation when the user initiates a drag gesture.
+     *
+     * This function determines which item in the LazyList is being dragged based on the
+     * starting offset of the drag gesture. It then stores the information about the
+     * dragged item and its index for use during the drag operation.
+     * */
+    fun onDragStart(offset: Offset) {
+        lazyListState.layoutInfo.visibleItemsInfo
+            .firstOrNull { item -> offset.y.toInt() in item.offset..item.offsetEnd }
+            ?.also {
+                initialDraggingElement = it
+                currentIndexOfDraggedItem = it.index
+            }
+    }
 
-				draggingDistance < 0 -> {
-					(startOffset - lazyListState.layoutInfo.viewportStartOffset).takeIf { diff -> diff < 0 }
-				}
+    /**
+     * Called when a drag operation is interrupted or canceled.
+     *
+     * Resets the state of the ongoing drag by clearing the dragged element reference,
+     * the dragged item index, and the dragging distance.  This is invoked when a drag
+     * is canceled, fails, or completes.
+     */
+    fun onDragInterrupted() {
+        initialDraggingElement = null
+        currentIndexOfDraggedItem = null
+        draggingDistance = 0f
+    }
 
-				else -> null
-			}
-		} ?: 0f
 
-		if (overscroll != 0f) {
-			scrollChannel.trySend(overscroll)
-		}
-	}
+    fun onDrag(offset: Offset) {
+        draggingDistance += offset.y
 
-	private fun LazyListState.getVisibleItemInfo(itemPosition: Int): LazyListItemInfo? {
-		return this.layoutInfo.visibleItemsInfo.getOrNull(itemPosition - this.firstVisibleItemIndex)
-	}
+        initialOffsets?.let { (top, bottom) ->
+            val startOffset = top.toFloat() + draggingDistance
+            val endOffset = bottom.toFloat() + draggingDistance
+            val middleOffset = (startOffset + endOffset) / 2f
 
-	private val LazyListItemInfo.offsetEnd: Int
-		get() = this.offset + this.size
+            currentElement?.let { current ->
+                val targetElement = lazyListState.layoutInfo.visibleItemsInfo.find {
+                    middleOffset.toInt() in it.offset..it.offsetEnd && current.index != it.index
+                }
+                if (targetElement != null) {
+                    currentIndexOfDraggedItem?.let {
+                        onMove.invoke(it, targetElement.index)
+                    }
+                    currentIndexOfDraggedItem = targetElement.index
+                } else {
+                    checkOverscroll()
+                }
+            }
+        }
+    }
+
+    private fun checkOverscroll() {
+        val overscroll = initialDraggingElement?.let {
+            val startOffset = it.offset + draggingDistance
+            val endOffset = it.offsetEnd + draggingDistance
+
+            return@let when {
+                draggingDistance > 0 -> {
+                    (endOffset - lazyListState.layoutInfo.viewportEndOffset).takeIf { diff -> diff > 0 }
+                }
+
+                draggingDistance < 0 -> {
+                    (startOffset - lazyListState.layoutInfo.viewportStartOffset).takeIf { diff -> diff < 0 }
+                }
+
+                else -> null
+            }
+        } ?: 0f
+
+        if (overscroll != 0f) {
+            scrollChannel.trySend(overscroll)
+        }
+    }
+
+    private fun LazyListState.getVisibleItemInfo(itemPosition: Int): LazyListItemInfo? {
+        return this.layoutInfo.visibleItemsInfo.getOrNull(itemPosition - this.firstVisibleItemIndex)
+    }
+
+    private val LazyListItemInfo.offsetEnd: Int
+        get() = this.offset + this.size
 }
